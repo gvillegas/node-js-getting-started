6bd130a3379789ef4cd986710f4f812e81800ff1
Giovanni Villegas <gvillegas@gmail.com>
2022-02-25 16:41:51 -0500

    testing db api;

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const PORT = process.env.PORT || 5000;
+
+/** API handlers */
+
+const homeHandler = async (req, res) => res.json({ home: true });
+
+const apiHandler = async (req, res) => {
+  let client;
+
+  try {
+    client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.json(results);
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  } finally {
+    try {
+      client.release();
+    } catch(e) {
+      // close quietly
+    }
+    
+  }
+};
+
+/** App */
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get("/", homeHandler)
+  .get("/api/6a095a", apiHandler)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
