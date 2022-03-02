f6b8c3472a87db0d140fbc1d1d3d9f340a1d2d9a
Giovanni Villegas <gvillegas@gmail.com>
2022-03-01 20:26:37 -0500

    fix: using EST timezone for call queries;

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const apiHandler = async (req, res) => {
 
   try {
     client = await pool.connect();
-    const sql = "SELECT ac.id, ac.first_name, ac.last_name, ac.phone, ac.agent_phone, TO_CHAR(ac.received_on_date, 'yyyy-MM-dd') as received_on_date, ac.received_on_time FROM all_calls ac WHERE received_on_date = NOW()::date";
+    const sql = "SELECT ac.id, ac.first_name, ac.last_name, ac.phone, ac.agent_phone, TO_CHAR(ac.received_on_date, 'yyyy-MM-dd') as received_on_date, ac.received_on_time FROM all_calls ac WHERE received_on_date = (NOW() at time zone 'EST')::date";
     const result = await client.query(sql);
     const results = { results: result ? result.rows : [] };
     res.json(results);
