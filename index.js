53639faa12fa6c6d4a5e8a60976973f75e01d659
Giovanni Villegas <gvillegas@gmail.com>
2022-03-02 21:33:10 -0500

    improv: exposing `/api/calls/daily` and `/api/countries/[phoneCode]`;

--- index.js
+++ index.js
@@ -12,30 +12,70 @@ const PORT = process.env.PORT || 5000;
 
 /** API handlers */
 
-const apiHandler = async (req, res) => {
+const dailyCallsApiHandler = async (req, res) => {
   let client;
 
   try {
     client = await pool.connect();
-    const sql = "SELECT ac.id, ac.first_name, ac.last_name, ac.phone, ac.agent_phone, TO_CHAR(ac.received_on_date, 'yyyy-MM-dd') as received_on_date, ac.received_on_time FROM all_calls ac WHERE received_on_date = (NOW() at time zone 'EST')::date";
+
+    const sql = `SELECT ac.id, ac.first_name, ac.last_name, ac.phone, ac.agent_phone, 
+      TO_CHAR(ac.received_on_date, 'yyyy-MM-dd') as received_on_date, ac.received_on_time 
+      FROM all_calls ac 
+      WHERE received_on_date = (NOW() at time zone 'EST')::date`;
+      
     const result = await client.query(sql);
-    const results = { results: result ? result.rows : [] };
-    res.json(results);
+    const responseBody = { results: result ? result.rows : [] };
+    res.json(responseBody);
   } catch (err) {
     console.error(err);
-    res.send(err);
+    res.status(500).send("Request cannot be handled, please retry later");
   } finally {
     try {
       client.release();
-    } catch(e) {
+    } catch (e) {
       // close quietly
     }
-    
+  }
+};
+
+const countriesApiHandler = async (req, res) => {
+  const { phoneCode } = req.params;
+
+  if (!phoneCode) {
+    return res.status(400).send("Country's phone code is required");
+  }
+
+  let client;
+
+  try {
+    client = await pool.connect();
+    const sql = "SELECT c.phonecode, c.nicename FROM countries c WHERE c.phonecode::text = $1";
+    const result = await client.query(sql, [phoneCode]);
+    let responseBody;
+
+    if (result && result.rows && result.rows.length) {
+      const { phonecode, nicename } = result.rows[0];
+      responseBody = { code: phonecode, name: nicename };
+    } else {
+      return res.status(404).send("Country code not found");
+    }
+
+    res.json(responseBody);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Request cannot be handled, please retry later");
+  } finally {
+    try {
+      client.release();
+    } catch (e) {
+      // close quietly
+    }
   }
 };
 
 /** App */
 
 express()
-  .get("/api/l33t", apiHandler)
+  .get("/api/calls/daily", dailyCallsApiHandler)
+  .get("/api/countries/:phoneCode", countriesApiHandler)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
